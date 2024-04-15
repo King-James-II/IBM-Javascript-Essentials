4a6aa83fa3226243897043174f4c1703356c74e1
King-James-II <jocundjames@gmail.com>
2024-04-15 12:58:48 -0400

    added pratice task for array iteration lab

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict- $22.99', 'Oatmeal- $21.99', 'Frittata- $15'];
+const mainCourseMenu = ['Steak- $24.99', 'Pasta- $19', 'Burger- $11.99', 'Salmon- $15'];
+const dessertMenu = ['Cake- $24,99', 'Ice Cream- $10.99', 'Pudding- $12', 'Fruit Salad- $14.99'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
